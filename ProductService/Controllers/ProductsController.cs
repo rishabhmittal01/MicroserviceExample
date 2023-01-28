6c2b1d578023f900dec953d21c6507193f8623fe
user <user@DESKTOP-5SI8BBK>
2023-01-28 17:10:20 +0530

    Add project files.

---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.Data;
+using ProductService.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppllcationDbContext _context;
+        public ProductsController(AppllcationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _context.Products.ToList();
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return _context.Products.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            try
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, product);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProductService/Data/AppllcationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Model;
+
+namespace ProductService.Data
+{
+    public class AppllcationDbContext : DbContext
+    {
+        public AppllcationDbContext(DbContextOptions
+            <AppllcationDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
